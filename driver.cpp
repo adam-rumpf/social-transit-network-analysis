637060e4e1ec7867d43a4e0527f2697d6461dbb3
Adam Rumpf <arumpf@hawk.iit.edu>
2019-11-20 16:17:58 -0500

    Updating README and adding placeholder driver file.

---
+++ driver.cpp
@@ -0,0 +1,22 @@
+/**
+Main driver for network analysis tools.
+
+This project includes submodules for analyzing various aspects of the input netowrk, including the following:
+-Evaluating whether the initial flow vector is "reasonable" by examining the flow:capacity ratio of each arc.
+-Evaluating the accessibility metric of every stop (using the population accessibility metric module, but treating the stops as population centers).
+-Selecting and constructing express route candidates.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+/// Main driver.
+int main()
+{
+	cout << "Hello, world!" << endl;
+
+	cin.get();
+
+	return 0;
+}
